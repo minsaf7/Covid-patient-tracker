8ca0156802294e0426e9f96f8595e176d9b10ef9
Minsaf <minsafmohamed7@gmail.com>
2020-08-22 00:48:39 -0700

    created loginViewController

---
+++ NIBM COVID19/View/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  NIBM COVID19
+//
+//  Created by Mohamed Minsaf on 8/22/20.
+//  Copyright © 2020 Mohamed Minsaf. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
