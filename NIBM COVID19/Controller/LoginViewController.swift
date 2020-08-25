03400128cbc971f5fac9713e42ba998ef1bdbcdb
Minsaf <minsafmohamed7@gmail.com>
2020-08-24 22:03:44 -0700

    added email and pword containers  to login commit

--- NIBM COVID19/Controller/LoginViewController.swift
+++ NIBM COVID19/Controller/LoginViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Mohamed Minsaf. All rights reserved.
 //
 
-import UIKit
+//import UIKit
 
 //
 //  LoginViewController.swift
@@ -32,13 +32,81 @@ class LoginViewController: UIViewController {
         
     }()
     
-    private  let emailViewContiner: UIView = {
+    let emailTextfield:UIView = {
+        let textfield = UITextField()
+       // textfield.borderStyle = .none
+      //  textfield.backgroundColor = .cyan
+        
+        textfield.borderStyle = .none
+               textfield.font = UIFont.systemFont(ofSize: 16)
+               textfield.textColor = .white
+               textfield.keyboardAppearance = .dark
+        textfield.attributedPlaceholder = NSAttributedString(string: "EMAIL", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        
+        return textfield
+        
+        
+        
+    }()
+    
+    let pwordtextfield:UIView = {
+        let pwordtextfield = UITextField()
+       // textfield.borderStyle = .none
+      //  textfield.backgroundColor = .cyan
+        
+        pwordtextfield.borderStyle = .none
+               pwordtextfield.font = UIFont.systemFont(ofSize: 16)
+               pwordtextfield.textColor = .white
+               pwordtextfield.keyboardAppearance = .dark
+        pwordtextfield.isSecureTextEntry = true
+        pwordtextfield.attributedPlaceholder = NSAttributedString(string: "PASSWORD", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        
+        return pwordtextfield
+        
+        
+        
+    }()
+    
+    
+    
+  private lazy var emailViewContailer: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
+       //view.backgroundColor = .white
+       // view.anchor( height: 60, width: 200)
+        
+        view.addSubview(emailTextfield)
+   emailTextfield.anchor( left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor,paddingLeft: 8, paddingBottom: 8, paddingRight: 8)
+  //  emailTextfield.anchor( left: view.leadingAnchor, bottom: view.bottomAnchor, right: view.rightAnchor,  paddingLeft: 8, paddingBottom: 8, paddingRight: 8)
         
         return view
     }()
+
     
+    
+    private lazy var pwordViewContiner: UIView = {
+           let view = UIView()
+           //view.backgroundColor = .black
+          // view.anchor( height: 60, width: 200)
+
+           view.addSubview(pwordtextfield)
+        pwordtextfield.anchor( left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8, paddingRight: 8)
+
+
+           return view
+       }()
+//
+//
+//    private let buttn :UIButton = {
+//
+//        let button = UIButton(type: .system)
+//         button.setTitle("Log In", for: .normal)
+//         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+//        // button.addTarget(self, action: #selector(handleSignIn), for: .touchUpInside)
+//         return button
+//    }()
+
     
     
     
@@ -54,17 +122,20 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         configureNavigationBar()
-        view.backgroundColor = .gray
+       // view.backgroundColor = .gray
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         view.addSubview(titleLabel)
         
-        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 40)
         titleLabel.centerX(inView: view)
         
+       view.addSubview(emailViewContailer)
+        emailViewContailer.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 20,  paddingRight: 20, height: 50)
         
-        
-        
+        view.addSubview(pwordViewContiner)
+        pwordViewContiner.anchor(top: emailViewContailer.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 20,  paddingRight: 20, height: 50)
+       
 
-        // Do any additional setup after loading the view.
     }
     
     
---
+++ NIBM COVID19/Controller/MainTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  MainTabBarController.swift
+//  NIBM COVID19
+//
+//  Created by Mohamed Minsaf on 8/22/20.
+//  Copyright © 2020 Mohamed Minsaf. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+   func configureTabBar(){
+        let homeTab = UINavigationController(rootViewController: LoginViewController(/* change the view controller to home */ ))
+    homeTab.tabBarItem.title = "Home"
+    
+    
+    
+    let updateTab = UINavigationController(rootViewController: LoginViewController(/* change the view controller to update */ ))
+      updateTab.tabBarItem.title = "Update"
+    
+    let settingTab = UINavigationController(rootViewController: LoginViewController(/* change the view controller to setting */ ))
+      settingTab.tabBarItem.title = "Setting"
+    
+    
+    }
+     
+     
+
+
+}
