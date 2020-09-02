3cae717e4bf5170283d3800c728c1e4add9bced0
Minsaf <minsafmohamed7@gmail.com>
2020-09-02 01:19:14 -0700

    uodated survey commit

--- NIBM COVID19/Controller/SurveyViewController.swift
+++ NIBM COVID19/Controller/SurveyViewController.swift
@@ -56,12 +56,12 @@ class SurveyViewController: UIViewController, UICollectionViewDelegate, UICollec
         
         self.view.addSubview(myCollectionView)
         
-        let que1 = Question( questionText: "What is 2 x 2 ?", options: ["2", "4", "8", "6"], correctAns: 1, wrongAns: -1, isAnswered: false)
-        let que2 = Question( questionText: "What is 4 + 2 ?", options: ["9", "4", "3", "6"], correctAns: 3, wrongAns: -1, isAnswered: false)
-        let que3 = Question( questionText: "What is 6 / 2 ?", options: ["2", "4", "3", "5"], correctAns: 2, wrongAns: -1, isAnswered: false)
-        let que4 = Question( questionText: "What is 2 - 2 ?", options: ["2", "4", "1", "0"], correctAns: 3, wrongAns: -1, isAnswered: false)
-        let que5 = Question( questionText: "What is 12 x 2 ?", options: ["24", "40", "26", "34"], correctAns: 0, wrongAns: -1, isAnswered: false)
-        let que6 = Question( questionText: "What is the color of sky?", options: ["Violet", "Yellow", "Blue", "White"], correctAns: 2, wrongAns: -1, isAnswered: false)
+        let que1 = Question( questionText: "Do you have any covid symptoms ?", options: ["Yes", "No"], correctAns: 1, wrongAns: -1, isAnswered: false)
+        let que2 = Question( questionText: "Do you have any covid symptoms ?", options: ["Yes", "No"], correctAns: 1, wrongAns: -1, isAnswered: false)
+        let que3 = Question( questionText: "Do you have any covid symptoms ?", options: ["Yes", "No"], correctAns: 1, wrongAns: -1, isAnswered: false)
+        let que4 = Question( questionText: "Do you have any covid symptoms ?", options: ["Yes", "No"], correctAns: 1, wrongAns: -1, isAnswered: false)
+        let que5 = Question( questionText: "Do you have any covid symptoms ?", options: ["Yes", "No"], correctAns: 1, wrongAns: -1, isAnswered: false)
+        let que6 = Question( questionText: "Do you have any covid symptoms ?", options: ["Yes", "No"], correctAns: 1, wrongAns: -1, isAnswered: false)
         questionsArray = [que1, que2, que3, que4, que5, que6]
         
         setupViews()
